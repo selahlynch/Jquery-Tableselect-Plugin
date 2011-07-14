453dc0db7ac5b920b9054a91d292153b32f0138f
Selah Lynch <selahlynch@gmail.com>
2011-07-14 11:39:12 -0400

    added code files to the repo

---
+++ demonstrate.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html dir="ltr" lang="en-US">
+
+<head>
+	<title>Table Select</title>
+	<style type="text/css">
+		.tableselect tr.selected {
+  			background-color: #4598FF;
+  		} 		
+  		.tableselect{
+  			font-family:Arial, Helvetica, sans-serif;
+  		}
+	</style>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="jquery.tablesorter.min.js"></script>
+	<script type="text/javascript" src="jquery.tablescroll.js"></script>
+	<script type="text/javascript" src="jquery.tableselect.js"></script>
+	<script type="text/javascript" >
+
+
+
+
+///////////////////// SEE THE CODE HERE! /////////////////////////////////
+	
+		$(document).ready(function(){
+			try{
+			$('#tbl3').tableselect();
+			$('#tbl4').tablesorter().tableselect({name:'thetabers', hidefirstrow:true}).tableScroll({height:100});				
+			}catch(err){
+				alert(err);
+			}
+		});	
+
+////////////////////////////////////////////////////////////////////////////
+		
+		
+		
+	</script>
+</head>
+
+<body >
+
+Posted variables:<br /> 
+<?php var_dump($_POST);?>
+<br /><br />
+
+<form method='post'>
+
+This table is selectable.
+<table id='tbl3' style=''>
+<thead>
+<tr><th>ID</th> <th>Name</th><th>Age</th></tr>
+</thead>
+<tbody>
+<tr><td>1</td> <td>Dan</td> <td>46</td></tr>
+<tr><td>2</td> <td>Sue</td> <td>54</td></tr>
+<tr><td>3</td> <td>Sadie</td> <td>21</td></tr>
+<tr><td>4</td> <td>Bob</td> <td>21</td></tr>
+<tr><td>5</td> <td>Jerry</td> <td>22</td></tr>
+<tr><td>6</td> <td>Stan</td> <td>31</td></tr>
+<tr><td>7</td> <td>Doug</td> <td>45</td></tr>
+<tr><td>8</td> <td>Matty</td> <td>23</td></tr>
+<tr><td>9</td> <td>Chaz</td> <td>1.3</td></tr>
+</tbody>
+</table> 
+<br />
+<hr />
+This table is sortable, scrollable, and selectable and has an item pre-selected.
+<table id='tbl4' style=''>
+<thead>
+<tr><th>ID</th> <th>Name</th><th>Age</th></tr>
+</thead>
+<tbody>
+<tr><td>1</td> <td>Dan</td> <td>46</td></tr>
+<tr class='selected'><td>2</td> <td>Sue</td> <td>54</td></tr>
+<tr><td>3</td> <td>Sadie</td> <td>21</td></tr>
+<tr><td>4</td> <td>Bob</td> <td>21</td></tr>
+<tr><td>5</td> <td>Jerry</td> <td>22</td></tr>
+<tr><td>6</td> <td>Stan</td> <td>31</td></tr>
+<tr><td>7</td> <td>Doug</td> <td>45</td></tr>
+<tr><td>8</td> <td>Matty</td> <td>23</td></tr>
+<tr><td>9</td> <td>Chaz</td> <td>1.3</td></tr>
+</tbody>
+</table> 
+<br />
+	
+<input type='submit' value='send_data'></input>
+	
+</form>
+	
+</body>
+
+</html>
---
+++ jquery.tableselect.js
@@ -0,0 +1,59 @@
+
+//jQuery Plugin - Tableselect
+//take the first table in any given jquery object, modify it to behave like a select 
+//when you select a row, the value from the first column will be posted 
+
+(function( $ ){
+	$.fn.tableselect = function( options ) {  
+
+		//get user options, use defaults if none given by user
+		var defaults = {name : 'tableselect', hidefirstcolumn : false};
+		var options = $.extend(defaults,options);
+		var postname = options.name;
+		
+		////////////////define methods///////////////////////
+		var tableselect_selectrow = function(jqtsdiv, jqrow){
+	  		jqtsdiv.find('tr:has("td")').removeClass('selected');
+	  		jqtsdiv.children().last().filter('input:hidden').remove();
+			jqrow.addClass('selected');
+			if(jqrow.length > 0){
+				var selectedval = jqrow.find('td:first').html();
+				jqtsdiv.append('<input type="hidden"></input>');
+				jqtsdiv.children().last().filter('input:hidden').val(selectedval).attr('name',postname);			
+			}
+		};
+		var tableselect_unselectall = function(jqtsdiv){
+	  		jqtsdiv.find('tr:has("td")').removeClass('selected');
+	  		jqtsdiv.children().last().filter('input:hidden').remove();			
+		};
+		///////////////////////////////////////////////////////
+		
+		
+		//select the first table only
+		var jq_firsttable = this.find('*').andSelf().filter('table:first');
+		if(!(jq_firsttable.length > 0)){
+		//	throw 'No tables founddd.';
+		}
+		jq_firsttable.wrap('<div class="tableselect"></div>');
+		jq_selecttable_div = jq_firsttable.parent();
+		
+		
+		//select all rows and add an onClick event that selects the row that was clicked on
+		jq_firsttable.find('tr:has("td")').click(function(){
+			tableselect_selectrow(jq_firsttable, $(this));
+		});
+		
+		//if user wants to preselect some rows, select first row with class selected
+		var jq_firstselrow = jq_firsttable.find('tr:has("td")').filter('.selected:first');
+		tableselect_selectrow(jq_firsttable, jq_firstselrow);
+
+		
+		//if user wants to hide the first column, do it
+		if(options.hidefirstcolumn == true){
+			jq_firsttable.find('tr').find('td:first').hide();
+			jq_firsttable.find('tr').find('th:first').hide();
+		}
+
+		return this;
+	};
+})( jQuery );
